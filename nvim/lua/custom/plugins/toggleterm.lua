b66e4efa7caae614335c694275133ef42384a98a
Nicholas Moore <nicholasvmoore@gmail.com>
2025-05-13 16:59:19 -0500

    Refactor neovim config into multiple files

---
+++ nvim/lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      start_in_insert = false,
+      persistent_mode = true,
+      shell = vim.o.shell,
+      auto_scroll = true,
+
+      -- Float options
+      float_opts = {
+        border = 'single',
+        title_pos = 'center',
+      },
+
+      -- Bindings
+      vim.keymap.set('n', '<leader>`', ':ToggleTerm direction=float<cr>', { desc = 'Open a floating terminal' }),
+    },
+  },
+}
